fd8cabed58e2c4c08b9c3fc525f14eb0c53575a3
NabihaIR <z5360710@ad.unsw.edu.au>
2024-10-01 10:42:48 +1000

    Updated rule change column

--- Data Product/NewRuleColumn.py
+++ Data Product/NewRuleColumn.py
@@ -6,12 +6,13 @@ file_path =(r"/Users/nabiharajput/Desktop/DATA3001/UNSW Data/2020-2023 Event Dat
 df = pd.read_csv(file_path)
 
 conditions = [
-    (df['RoundId'] < 9) & (df['SeasonId'] == 2020),  # If RoundId < 9 and Year = 2020
-    (df['SeasonId'] < 2022) & (df['RoundId'] >= 9),  # If Year < 2022 and RoundId >= 9
-    (df['SeasonId'] >= 2022)                         # If Year >= 2022
+    (df['SeasonId'] == 2020) & (df['RoundId'] < 9),   # If Year = 2020 and RoundId < 9
+    (df['SeasonId'] == 2020) & (df['RoundId'] >= 9),  # Elif Year = 2020 and RoundId >= 9
+    (df['SeasonId'] == 2021),                         # Elif Year = 2021
+    (df['SeasonId'] >= 2022)                          # Elif Year >= 2022
 ]
 
-values = [0, 1, 2]  # 0 for first condition, 1 for second, 2 for third
+values = [0, 1, 1, 2]  # 0 for first condition, 1 for second, 2 for third
 
 df['RuleChange'] = np.select(conditions, values, default=np.nan)  # Else: NA (NaN in pandas)
 
