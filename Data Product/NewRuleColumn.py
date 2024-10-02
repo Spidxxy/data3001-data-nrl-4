564fbd47c79913b5f2307d60925b0312ba871d05
NabihaIR <z5360710@ad.unsw.edu.au>
2024-10-03 09:44:56 +1000

    Minor changes to rule change column

--- Data Product/NewRuleColumn.py
+++ Data Product/NewRuleColumn.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 # Load the CSV file
-file_path =(r"/Users/nabiharajput/Desktop/DATA3001/UNSW Data/2020-2023 Event Data.csv") # Replace with your CSV file path if ur working on/copying this
-df = pd.read_csv(file_path)
+file_path =(r"/Users/nabiharajput/Desktop/DATA3001/UNSW Data/Data Product/2020-2023 PTB and Tackle Filtered.csv") # Replace with your CSV file path if ur working on/copying this
+df = pd.read_csv(file_path, low_memory=False)
 
 conditions = [
     (df['SeasonId'] == 2020) & (df['RoundId'] < 9),   # If Year = 2020 and RoundId < 9
@@ -14,6 +14,7 @@ conditions = [
 
 values = [0, 1, 1, 2]  # 0 for first condition, 1 for second, 2 for third
 
-df['RuleChange'] = np.select(conditions, values, default=np.nan)  # Else: NA (NaN in pandas)
+df['RuleChange'] = np.select(conditions, values, default=-1).astype(int)  # Else: NA (NaN in pandas)
 
-df.to_csv(r'/Users/nabiharajput/Desktop/DATA3001/UNSW Data/Data Product/2020-2023 PTB and Tackle Filtered.csv', index=False)
+df.to_csv(r'/Users/nabiharajput/Desktop/DATA3001/UNSW Data/Data Product/2020-2023 PTB and Tackle Filtered_newColumn.csv', index=False)
+
