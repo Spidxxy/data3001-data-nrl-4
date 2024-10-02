9a615af7c14b52501e8ed1c52964c8453c403216
na-bee-s <z5360710@ad.unsw.edu.au>
2024-10-03 09:42:54 +1000

    updated rule change column

--- Data Product/NewRuleChange.py
+++ Data Product/NewRuleChange.py
@@ -2,17 +2,18 @@ import pandas as pd
 import numpy as np
 
 # Load the CSV file
-file_path =(r"/Users/nabiharajput/Desktop/DATA3001/UNSW Data/2020-2023 Event Data.csv") # Replace with your CSV file path if ur working on/copying this
-df = pd.read_csv(file_path)
+file_path =(r"/Users/nabiharajput/Desktop/DATA3001/UNSW Data/Data Product/2020-2023 PTB and Tackle Filtered.csv") # Replace with your CSV file path if ur working on/copying this
+df = pd.read_csv(file_path, low_memory=False)
 
 conditions = [
-    (df['RoundId'] < 9) & (df['SeasonId'] == 2020),  # If RoundId < 9 and Year = 2020
-    (df['SeasonId'] < 2022) & (df['RoundId'] >= 9),  # If Year < 2022 and RoundId >= 9
-    (df['SeasonId'] >= 2022)                         # If Year >= 2022
+    (df['SeasonId'] == 2020) & (df['RoundId'] < 9),   # If Year = 2020 and RoundId < 9
+    (df['SeasonId'] == 2020) & (df['RoundId'] >= 9),  # Elif Year = 2020 and RoundId >= 9
+    (df['SeasonId'] == 2021),                         # Elif Year = 2021
+    (df['SeasonId'] >= 2022)                          # Elif Year >= 2022
 ]
 
-value = [0, 1, 2]  # 0 for first condition, 1 for second, 2 for third
+values = [0, 1, 1, 2]  # 0 for first condition, 1 for second, 2 for third
 
-df['RuleChange'] = np.select(conditions, value, default=np.nan)  # Else: NA (NaN in pandas)
+df['RuleChange'] = np.select(conditions, values, default=-1).astype(int)  # Else: NA (NaN in pandas)
 
-df.to_csv(r'/Users/nabiharajput/Desktop/DATA3001/UNSW Data/Data Product/2020-2023 PTB and Tackle Filtered.csv', index=False)
+df.to_csv(r'/Users/nabiharajput/Desktop/DATA3001/UNSW Data/Data Product/2020-2023 PTB and Tackle Filtered_newColumn.csv', index=False)
